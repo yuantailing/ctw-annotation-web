dc9899d33a040c08660b0e5bcd5b50c250489f6c
yuantailing <yuantailing@126.com>
2016-11-30 03:59:30 +0800

    FIX use QuerySet instead of raw sql

--- collection/scripts/make_image_packages.py
+++ collection/scripts/make_image_packages.py
@@ -13,19 +13,14 @@ def run(*args):
         print 'Usage: python manage.py runscript make_image_packages --traceback --script-args <package_size>'
     assert(len(args) == 1)
     package_size = int(args[0])
-    # direction_set = Image.objects.filter(package=None).values('direction').annotate(count_direction=Count('pk'))
-    direction_set = Image.objects.raw("SELECT id, direction, COUNT(id) AS count_direction FROM collection_image GROUP BY direction")
-    direction_set = list(direction_set)
-    print direction_set
-    # direction_set = filter(lambda d: d['count_direction'] >= package_size, direction_set)
-    direction_set.sort(cmp=lambda a, b: 1 if a.direction > b.direction else -1)
+    direction_set = Image.objects.filter(package=None).values('direction').annotate(count_direction=Count('pk')).order_by('direction')
     for direction_obj in direction_set:
-        direction  = direction_obj.direction
-        cnt = direction_obj.count_direction
+        direction  = direction_obj['direction']
+        cnt = direction_obj['count_direction']
         print "direction %d, %d images => %d packages (%d have no package, %d ignored)" % (direction, cnt / package_size * package_size, cnt / package_size, cnt, cnt % package_size)
     assert(yesno("confirm?"))
     for direction_obj in direction_set:
-        direction  = direction_obj.direction
+        direction  = direction_obj['direction']
         images = Image.objects.filter(package=None, direction=direction).order_by('number').values_list('pk', flat=True)
         cnt = 0
         if (len(images) >= package_size):
@@ -33,4 +28,4 @@ def run(*args):
                 package = Package.objects.create(direction=direction)
                 Image.objects.filter(pk__in=images[start:start + package_size]).update(package=package)
                 cnt += 1
-        assert(cnt == direction_obj.count_direction / package_size)
+        assert(cnt == direction_obj['count_direction'] / package_size)
