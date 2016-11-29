078994fe7aaeed5763c0fb3a6f120c79a3583284
yuantailing <yuantailing@126.com>
2016-11-29 18:38:56 +0800

    FEAT app accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,15 +5,18 @@
 
 from django.conf.urls import url
 from django.contrib.auth import views
+from django.shortcuts import render
 
+app_name = 'accounts'
 urlpatterns = [
-    url(r'^login/$', views.login, {'template_name': 'admin/login.html', 'redirect_authenticated_user': True}, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
-    url(r'^password_change/$', views.password_change, name='password_change'),
+    url('^$', lambda request: render(request, 'accounts/index.html'), name='index'),
+    url(r'^login/$', views.login, {'template_name': 'accounts/login.html', 'redirect_authenticated_user': True}, name='login'),
+    url(r'^logout/$', views.logout, {'template_name': 'accounts/logged_out.html'}, name='logout'),
+    url(r'^password_change/$', views.password_change, {'post_change_redirect': 'accounts:password_change_done'}, name='password_change'),
     url(r'^password_change/done/$', views.password_change_done, name='password_change_done'),
-    url(r'^password_reset/$', views.password_reset, name='admin_password_reset'),
+    url(r'^password_reset/$', views.password_reset, {'email_template_name': 'accounts/password_reset_email.html', 'post_reset_redirect': 'accounts:password_reset_done'}, name='admin_password_reset'),
     url(r'^password_reset/done/$', views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.password_reset_confirm, name='password_reset_confirm'),
+        views.password_reset_confirm, {'post_reset_redirect': 'accounts:password_reset_complete'}, name='password_reset_confirm'),
     url(r'^reset/done/$', views.password_reset_complete, name='password_reset_complete'),
 ]
